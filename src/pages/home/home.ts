117da76b4ffd9a26da1b068a855ff34ce0a97f2d
Callio <cedricallio@yahoo.fr>
2017-05-08 20:09:55 +0200

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from 'ionic-angular';
+ 
+@Component({
+	selector: 'page-home',
+	templateUrl: 'home.html'
+})
+
+export class HomePage {
+ 
+    @ViewChild('signupSlider') signupSlider: any;
+ 
+    slideOneForm: FormGroup;
+    slideTwoForm: FormGroup;
+ 
+    submitAttempt: boolean = false;
+ 
+    constructor(public navCtrl: NavController, public formBuilder: FormBuilder) { 
+		this.slideOneForm = formBuilder.group({
+			firstName: [''],
+			lastName: [''],
+			age: ['']
+		});
+	}
+ 
+    next(){
+        this.signupSlider.slideNext();
+    }
+ 
+    prev(){
+		this.signupSlider.slidePrev();
+    }
+ 
+    save(){
+ 
+    }
+ 
+}
